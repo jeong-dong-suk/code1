4db8de7f628260c26854dd6fd1b45fd8ef75665b
jeong-dong-suk <102285071+jeong-dong-suk@users.noreply.github.com>
2023-03-25 15:29:20 +0900

    test3

---
+++ BinarySearchTree3.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "BinaryTree3.h"
+
+typedef BTData	BSTData;
+
+void BSTMakeAndInit(BTreeNode** pRoot);
+
+BSTData BSTGetNodeData(BTreeNode* bst);
+
+void BSTInsert(BTreeNode** pRoot, BSTData data);
+
+
+BTreeNode* BSTSearch(BTreeNode* bst, BSTData target);
+
+ 
+BTreeNode* BSTRemove(BTreeNode** pRoot, BSTData target);
+
+
+void BSTShowAll(BTreeNode* bst);
+
